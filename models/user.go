e5f26523bad9cf7218585fe3676d17696f863978
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-06-25 13:40:22 +0000

    refactor user model to add password field

--- models/user.go
+++ models/user.go
@@ -4,7 +4,8 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Name  string `json:"name"`
-	Email string `json:"email"`
-	Age   int    `json:"age"`
+	Name     string `json:"name"`
+	Email    string `json:"email" gorm:"unique" binding:"required"`
+	Age      int    `json:"age"`
+	Password string `json:"password" binding:"required"`
 }
