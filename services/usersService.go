a842b120c259d6f7410e9d45014e60dc00cb48cd
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-07-01 07:46:28 +0000

    refactor the service layer to make use of the repository

--- services/usersService.go
+++ services/usersService.go
@@ -1,1 +1,55 @@
 package services
+
+import (
+	"users-rest-api-gorm/dto"
+	"users-rest-api-gorm/models"
+	"users-rest-api-gorm/repository"
+)
+
+type UserService struct {
+	repository repository.UserRepository
+}
+
+func NewUserService() *UserService {
+	return &UserService{repository: repository.UserRepository{}}
+}
+
+func (s *UserService) CreateUser(theUser models.User) (*models.User, error) {
+	createdUser, err := s.repository.CreateUser(theUser)
+	if err != nil {
+		return nil, err
+	}
+	return createdUser, nil
+}
+
+func (s *UserService) GetAllUsers() ([]models.User, error) {
+	users, err := s.repository.GetAllUsers()
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func (s *UserService) GetUserById(id int) (*models.User, error) {
+	foundUser, err := s.repository.GetUserByID(id)
+	if err != nil {
+		return nil, err
+	}
+	return foundUser, nil
+}
+
+func (s *UserService) UpdateUser(id int, updated dto.UpdateUserDTO) (*models.User, error) {
+	user, err := s.repository.UpdateUser(id, updated)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (s *UserService) DeleteUser(id int) error {
+	err := s.repository.DeleteUser(id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
