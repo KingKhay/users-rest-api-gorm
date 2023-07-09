7fba928a20586d6261506f02e89d0d5c676c1440
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-07-09 20:26:00 +0000

    refactor login dto

--- dto/logindto.go
+++ dto/logindto.go
@@ -2,5 +2,5 @@ package dto
 
 type LoginDTO struct {
 	Email    string `json:"email" binding:"required"`
-	Password string `json:"password" binding:"required`
+	Password string `json:"password" binding:"required"`
 }
